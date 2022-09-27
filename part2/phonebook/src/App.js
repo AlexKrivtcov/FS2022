3dd43f8ed70c2a3a627ce7ef23b6f81f1d72d23c
Sasha <alkrivtcov@gmail.com>
2022-09-26 18:01:15 -0700

    2.8: The Phonebook, step3

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,12 +2,12 @@ import { useState } from 'react'
 
 
 
-const Person = ({person}) => <td>{person.name}</td>
+const Person = ({person}) => <tr><td>{person.name}</td><td>{person.number}</td></tr>
 const Persons = ({persons}) => {
   return (
     <table>
       <tbody>
-        {persons.map(person => <tr key={person.id}><Person person={person}/></tr>)}
+        {persons.map(person => <Person key={person.id} person={person}/>)}
       </tbody>
     </table>
   )
@@ -20,7 +20,7 @@ const App = () => {
       id: 1 }
   ]) 
   const [newName, setNewName] = useState('')
-  
+  const [newNumber, setNewNumber] = useState('')
 
   const addName = (event) => {
     event.preventDefault()
@@ -28,7 +28,8 @@ const App = () => {
     
     const newPerson = {
       id: persons.length + 1,
-      name: newName
+      name: newName,
+      number: newNumber
     }
     const personsNames = persons.map(person => person.name.toLowerCase())
     console.log('list of names', personsNames)
@@ -39,20 +40,27 @@ const App = () => {
     if (nameAlreadyExists) {
       alert(`${newName} is already added to the phonebook`)
     } 
+    else if (!newNumber) {
+      alert(`Number field is empty`)
+    }
     else {
       //console.log(newPerson, 'was added to the contacts');
-      console.log(newName, 'was added to the contacts');
+      console.log(newName, 'was added to the contacts with phone #', newNumber);
       setPersons(persons.concat(newPerson))
+      setNewNumber('')
       setNewName('')
     }
     
   }
 
   const handleNameChange = (event) => {
-    //console.log('name changed to ', event.target.value)
+    console.log('name changed to ', event.target.value)
     setNewName(event.target.value)
   }
-
+  const handleNumberChange = (event) => {
+    console.log('number changed to ', event.target.value)
+    setNewNumber(event.target.value)
+  }
 
   return (
     <div>
@@ -65,6 +73,14 @@ const App = () => {
             onChange={handleNameChange}
             value={newName}
             type="text"/>
+        </div>
+        <div>
+          <label htmlFor="newNumber">number: </label>
+          <input 
+            id="newNumber" 
+            onChange={handleNumberChange}
+            value={newNumber}
+            type="tel"/>
         </div>
         <div>
           <button type="submit" disabled={Boolean(!newName)} >add</button>
