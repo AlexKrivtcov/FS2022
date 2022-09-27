efe6156d065ab8916d4ff827627118a8faf811cd
Sasha <alkrivtcov@gmail.com>
2022-09-26 17:32:29 -0700

    2.7: The Phonebook, step2

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,21 +1,46 @@
 import { useState } from 'react'
 
+
+
+const Person = ({person}) => <td>{person.name}</td>
+const Persons = ({persons}) => {
+  return (
+    <table>
+      <tbody>
+        {persons.map(person => <tr key={person.id}><Person person={person}/></tr>)}
+      </tbody>
+    </table>
+  )
+}
+
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas',
       id: 1 }
   ]) 
   const [newName, setNewName] = useState('')
+  
 
   const addName = (event) => {
     event.preventDefault()
     console.log('button clicked', event.target)
+    
     const newPerson = {
       id: persons.length + 1,
       name: newName
     }
-    if (newName){
-      console.log(newPerson, 'was added to the contacts');
+    const personsNames = persons.map(person => person.name.toLowerCase())
+    console.log('list of names', personsNames)
+    
+    const nameAlreadyExists = personsNames.includes(newName.toLowerCase())
+    console.log('Name already exists', nameAlreadyExists)
+
+    if (nameAlreadyExists) {
+      alert(`${newName} is already added to the phonebook`)
+    } 
+    else {
+      //console.log(newPerson, 'was added to the contacts');
       console.log(newName, 'was added to the contacts');
       setPersons(persons.concat(newPerson))
       setNewName('')
@@ -24,20 +49,10 @@ const App = () => {
   }
 
   const handleNameChange = (event) => {
-    console.log(event.target.value)
+    //console.log('name changed to ', event.target.value)
     setNewName(event.target.value)
   }
-  const Person = ({person}) => <td>{person.name}</td>
-  const Persons = ({persons}) => {
-    console.log(persons)
-    return (
-      <table>
-        <tbody>
-          {persons.map(person => <tr key={person.id}><Person person={person}/></tr>)}
-        </tbody>
-      </table>
-    )
-  }
+
 
   return (
     <div>
@@ -52,7 +67,7 @@ const App = () => {
             type="text"/>
         </div>
         <div>
-          <button type="submit">add</button>
+          <button type="submit" disabled={Boolean(!newName)} >add</button>
         </div>
       </form>
       <h2>Numbers</h2>
