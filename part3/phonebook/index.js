8ad58d4fc01d37fb065fc3619a82e69eb638d180
Sasha <alkrivtcov@gmail.com>
2022-10-05 17:44:24 -0700

    3.4: Phonebook backend, step4

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -48,19 +48,24 @@ app.get('/info', (request, response) => {
 })
 app.get('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
-    console.log(id)
+    //console.log(id)
     const person = persons.find(person => person.id === id)
     console.log(person)
     if (person) {
         response.json(person)
-      } else {
+    } else {
         response.status(404).send(`Sorry, The person with the ID <strong>${id}</strong> doesn't exist`).end()
-      }
+    }
+})
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+  
+    response.status(204).end()
   })
-
 
 
 const PORT = 3001
 app.listen(PORT, () => {
 console.log(`Server running on port ${PORT}`)
-})
+}) 
