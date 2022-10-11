e6db9897ca3b0d41b8f9e93fb36383cd8481d0b7
Sasha <alkrivtcov@gmail.com>
2022-10-11 16:07:52 -0700

    3.10: phonebook backend, step10

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 
 app.use(express.json())
 app.use(cors())
-app.use(express.static('build'))
+
 
 
 morgan.token('profile', function getData(request, response) {
@@ -118,7 +118,7 @@ app.post('/api/persons', (request, response) => {
   })
   
 
-const PORT = 3001
-app.listen(PORT, () => {
-console.log(`Server running on port ${PORT}`)
-}) 
+  const PORT = process.env.PORT || 3001
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
