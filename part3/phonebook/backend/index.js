92cc7cc0196ac09ae56dddc7d3f8af5bb3c3692c
Sasha <alkrivtcov@gmail.com>
2022-10-14 13:37:34 -0700

    3.15: Phonebook database, step3

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -1,14 +1,16 @@
 require('dotenv').config()
 
 const express = require('express')
-const morgan = require('morgan')
 const app = express()
 const cors = require('cors')
 const Person = require('./models/person')
 
-app.use(express.json())
-app.use(cors())
+
 app.use(express.static('build'))
+app.use(express.json())
+const morgan = require('morgan')
+app.use(cors())
+
 
 
 morgan.token('profile', function getData(request, response) {
@@ -18,35 +20,6 @@ morgan.token('profile', function getData(request, response) {
     return " "
 })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :profile'))
-// app.use(morgan(':profile'))
-let persons = [
-    { 
-        "id": 1,
-        "name": "Arto Hellas", 
-        "number": "040-123456"
-      },
-      { 
-        "id": 2,
-        "name": "Ada Lovelace", 
-        "number": "39-44-5323523"
-      },
-      { 
-        "id": 3,
-        "name": "Dan Abramov", 
-        "number": "12-43-234345"
-      },
-      { 
-        "id": 4,
-        "name": "Mary Poppendieck", 
-        "number": "39-23-6423122"
-      }
-]
-
-const generateId = () => {
-    const randomId = Math.floor(Math.random()*1000000)
-    //console.log(randomId)
-    return randomId
-}
 
 const info = (count) => {
     const currentDate = new Date()    
@@ -89,10 +62,11 @@ app.get('/api/persons/:id', (request, response) => {
 
 })
 app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-  
-    response.status(204).end()
+    Person.findByIdAndRemove(request.params.id)
+        .then(result =>{
+            response.status(204).end()
+        })
+        .catch(error =>next(error))
 })
 
 app.post('/api/persons', (request, response) => {
