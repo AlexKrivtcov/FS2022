33f70a406aacbe000b3973198075615f2b1a9c9b
Sasha <alkrivtcov@gmail.com>
2022-10-25 13:30:18 -0700

    4.8: Blog list tests, step1

---
+++ part4/blog/backend/tests/blog_api.test.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const helper = require('./test_helper')
+
+const api = supertest(app)
+
+const Blog = require('../models/blog')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  let blogObject = new Blog(helper.initialBlogs[0])
+  await blogObject.save()
+
+  blogObject = new Blog(helper.initialBlogs[1])
+  await blogObject.save()
+})
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('all blogs are returned', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body).toHaveLength(helper.initialBlogs.length)
+})
+
+test('a specific blog\'s title is within the returned blogs', async () => {
+  const response = await api.get('/api/blogs')
+
+  const titles = response.body.map(r => r.title)
+  expect(titles).toContain('React patterns')
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
